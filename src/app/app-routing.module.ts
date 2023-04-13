9a997f4560520800c3a6099fe0e4f8c13c11b4ba
Yee <91130566+Yaksha05@users.noreply.github.com>
2023-04-13 13:43:47 +0800

    Responsive Employee-management

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { EmployeeManagementComponent } from './employee-management/employee-management.component';
+import { combineLatest } from 'rxjs';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{
+  path: 'employee',
+  component: EmployeeManagementComponent
+}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { EmployeeManagementComponent } from './employee-management/employee-management.component';
+import { AddStaffComponent } from './add-staff/add-staff.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
+    EmployeeManagementComponent,
+    AddStaffComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     MaterialModule
   ],
   providers: [],
