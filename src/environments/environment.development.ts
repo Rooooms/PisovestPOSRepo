6306eb5aab52b9b977e81ad9a17b262431f57e46
John Edward D. Complido <91130909+Drawde0731@users.noreply.github.com>
2023-04-26 10:01:35 +0800

    baseAPIURL dinoble ko

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production : true,
-    baseApiUrl : 'http://localhost:5125/'
+    baseApiUrl : 'http://localhost:5125/',
+    baseAPIURL : 'http://localhost:5125/'
 };
